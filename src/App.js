2214164053e0d77bb4ffb790a8af184eec824a00
Ethmane Didi Bilal <blueskytech@Ethmanes-MBP.home>
2024-02-20 21:23:54 +0100

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { SettingsProvider } from "./context/SettingsContext";
 import { AuthProvider } from "./context/AuthProvider";
 import Sidebar from "./components/Sidebar";
 import Header from "./components/Header";
 import Home from "./components/Home";
-
 import Settings from "./components/Settings";
 import ContactUs from "./components/ContactUs";
 import HelpFaq from "./components/HelpFaq";
@@ -17,39 +16,49 @@ import Register from "./components/Register";
 import Dashboard from "./components/Dashboard";
 import UserProfile from "./components/UserProfile";
 import Analytics from "./components/Analytics";
+import { ThemeProvider } from "./context/ThemeContext";
 
 function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
   return (
     <AuthProvider>
       <SettingsProvider>
-        <Router>
-          <div className="flex flex-col min-h-screen">
-            <div className="flex flex-1">
-              <Sidebar />
-              <div className="flex-1 flex flex-col">
-                <Header />
-                <main className="flex-1">
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    {/* <Route path="/chart" element={<Chart />} /> */}
-                    <Route path="/settings" element={<Settings />} />
-                    <Route path="/contact" element={<ContactUs />} />
-                    <Route path="/help" element={<HelpFaq />} />
-                    <Route path="/terms" element={<TermsOfService />} />
-                    <Route path="/privacy" element={<PrivacyPolicy />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
-                    <Route path="/profile" element={<UserProfile />} />
-                    {/* Add the route for Analytics */}
-                    <Route path="/analytics" element={<Analytics />} />
-                  </Routes>
-                </main>
-                <Footer />
+        <ThemeProvider>
+          <Router>
+            <div className="flex flex-col min-h-screen">
+              <div className="flex">
+                <Sidebar
+                  isSidebarOpen={isSidebarOpen}
+                  toggleSidebar={toggleSidebar}
+                />
+                <div className="flex-1">
+                  <Header toggleSidebar={toggleSidebar} />
+                  <main className="flex-1">
+                    <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/dashboard" element={<Dashboard />} />
+                      <Route path="/settings" element={<Settings />} />
+                      <Route path="/contact" element={<ContactUs />} />
+                      <Route path="/help" element={<HelpFaq />} />
+                      <Route path="/terms" element={<TermsOfService />} />
+                      <Route path="/privacy" element={<PrivacyPolicy />} />
+                      <Route path="/login" element={<Login />} />
+                      <Route path="/register" element={<Register />} />
+                      <Route path="/profile" element={<UserProfile />} />
+                      {/* Add the route for Analytics */}
+                      <Route path="/analytics" element={<Analytics />} />
+                    </Routes>
+                  </main>
+                  <Footer />
+                </div>
               </div>
             </div>
-          </div>
-        </Router>
+          </Router>
+        </ThemeProvider>
       </SettingsProvider>
     </AuthProvider>
   );
