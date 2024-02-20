877c509934bfca06bb863821e6c3aa5c53ee1dc5
Ethmane Didi Bilal <blueskytech@Ethmanes-MBP.home>
2024-02-20 11:16:43 +0100

    Add routes directory

---
+++ routes/authRoutes.js
@@ -0,0 +1,13 @@
+// // authRoutes.js
+
+// const express = require("express");
+// const router = express.Router();
+// const { registerUser, loginUser } = require("../controllers/authController");
+
+// // Define route for user registration
+// router.post("/register", registerUser);
+
+// // Define route for user login
+// router.post("/login", loginUser);
+
+// module.exports = router;
---
+++ routes/userRoutes.js
@@ -0,0 +1,11 @@
+// const express = require("express");
+// const router = express.Router();
+// const UserController = require("../controllers/UserController");
+
+// // Get user dashboard configuration
+// router.get("/:userId/dashboard", UserController.getDashboardConfig);
+
+// // Update user dashboard configuration
+// router.put("/:userId/dashboard", UserController.updateDashboardConfig);
+
+// module.exports = router;
