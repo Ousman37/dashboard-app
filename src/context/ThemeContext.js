9f754b5b356fce6d581503970857c14cd764205d
Ethmane Didi Bilal <blueskytech@Ethmanes-MBP.home>
2024-02-20 21:25:16 +0100

    Add ThemeContext module

---
+++ src/context/ThemeContext.js
@@ -0,0 +1,37 @@
+// src/context/ThemeContext.js
+import React, { createContext, useContext, useState, useEffect } from "react";
+
+const ThemeContext = createContext();
+
+export const useTheme = () => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light"); // Default theme is light
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+    const initialTheme = root.classList.contains("dark") ? "dark" : "light";
+    setTheme(initialTheme);
+  }, []);
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => {
+      const newTheme = prevTheme === "light" ? "dark" : "light";
+      const root = window.document.documentElement;
+
+      if (newTheme === "dark") {
+        root.classList.add("dark");
+      } else {
+        root.classList.remove("dark");
+      }
+
+      return newTheme;
+    });
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
