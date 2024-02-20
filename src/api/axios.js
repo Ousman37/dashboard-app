5eb00cbba731944dde90271579d1b274771a44b9
Ethmane Didi Bilal <blueskytech@Ethmanes-MBP.home>
2024-02-20 11:16:58 +0100

    Add API directory

---
+++ src/api/axios.js
@@ -0,0 +1,9 @@
+// // axios.js
+
+// import axios from "axios";
+
+// const instance = axios.create({
+//   baseURL: "http://localhost:5001/api", // Update the port number if necessary
+// });
+
+// export default instance;
