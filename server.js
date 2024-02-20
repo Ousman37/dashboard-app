d6675481076ab22b60f6febe4e889e85c5b7afc1
Ethmane Didi Bilal <blueskytech@Ethmanes-MBP.home>
2024-02-20 11:16:50 +0100

    Add server file

---
+++ server.js
@@ -0,0 +1,34 @@
+//server.js
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+// const userRoutes = require("./routes/userRoutes");
+// const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+
+// Simplified CORS setup
+app.use(cors());
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error("MongoDB Connection Error:", err));
+
+// API routes
+// app.use("/api/users", userRoutes);
+// app.use("/api/auth", authRoutes);
+
+// Start the server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
